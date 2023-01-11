eab461de167f029572d86ee0b3e2c0bf83a6c28e
michoel kahana <kmichoel25@gmail.com>
2023-01-11 12:46:19 -0500

    commit tree 544d17cfdaa63a4e73840d92effd38e62d56f924

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    
+    function handleFocus() {
+        console.log('Good!')
+    }
+
+    function handleBLur() {
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <button onFocus={handleFocus} onBlur={handleBLur}>
+        Eyes on me
+    </button>
+  )
+}
